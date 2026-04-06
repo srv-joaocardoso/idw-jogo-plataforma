d6846a669d4f1d5b6e4c2d8c1c6423cf506451cf
srv-joaocardoso <srv.joaocardoso@gmail.com>
2026-04-06 12:57:44 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,186 @@
+
+class Jogo {
+    constructor() {
+        this.dimensoes = {
+            largura: 400,
+            altura: 500
+        }
+
+        this.jogador = new Jogador()
+        this.plataformas = []
+        this.posicaoMouseX = 0
+
+        this.AreaDoJogo = this.NovoJogo()
+        this.AdicionarRastreioDoMouse()
+        this.InicializarPlataformas()
+        this.Renderizar()
+    }
+
+    NovoJogo() {
+        this.main = document.querySelector('main')
+        this.main.style.width = this.dimensoes.largura + "px"
+        this.main.style.height = this.dimensoes.altura + "px"
+        return this.main
+    }
+
+    VerificarColisao(el1, el2) {
+        const r1 = el1.getBoundingClientRect()
+        const r2 = el2.getBoundingClientRect()
+
+        return !(
+            r1.right < r2.left ||
+            r1.left > r2.right ||
+            r1.bottom < r2.top ||
+            r1.top > r2.bottom
+        )
+    }
+
+    AdicionarRastreioDoMouse() {
+        document.addEventListener("mousemove", ({ clientX }) => {
+            this.posicaoMouseX = clientX;
+        });
+    }
+
+    InicializarPlataformas() {
+        this.primeiraPlataforma = new Plataforma(this.jogador.posicao.x, this.dimensoes.altura - this.jogador.dimensoes.altura)
+
+        this.plataformas.push(this.primeiraPlataforma)
+        this.intervaloEntrePlataformas = this.jogador.alturaPulo * 0.9
+
+        
+            Array(Math.floor(this.dimensoes.altura / this.intervaloEntrePlataformas))
+                .fill()
+                .forEach((_, i) => this.plataformas.push(new Plataforma(Math.random() * (
+                    this.dimensoes.largura - this.primeiraPlataforma.dimensoes.largura
+                ), this.primeiraPlataforma.posicao.y - ((i + 1) * this.intervaloEntrePlataformas)
+            )))
+    }
+
+    Renderizar() {
+        this.jogador.Animar()
+
+        this.jogador.posicao.x = this.posicaoMouseX
+
+        const elementos = [this.jogador].concat(this.plataformas)
+
+        elementos.forEach(elemento => {
+            if (this.jogador.posicao.y < this.dimensoes.altura / 2) {
+                elemento.posicao.y += 4
+                if (elemento.classe == "jogador") elemento.yBase += 4
+            }
+
+            if (!this.VerificarColisao(elemento.elemento, this.AreaDoJogo)) {
+                if (elemento.classe == "plataforma") {
+                    elemento.posicao.y = 20
+                }
+
+                if (elemento.classe == "jogador") {
+                    this.jogador.elemento.remove()
+                    this.jogador = new Jogador()
+                }
+            }
+
+            if (elemento.classe == "plataforma" && this.jogador.estado == "caindo" && this.VerificarColisao(this.jogador.elemento, elemento.elemento)) {
+                this.jogador.AplicarPulo()
+            }
+
+            elemento.Renderizar()
+        })
+
+        requestAnimationFrame(() => this.Renderizar())
+    }
+}
+
+class ObjetoDoJogo {
+    constructor() {
+        this.posicao = {
+            x: 0,
+            y: 0
+        }
+
+        this.dimensoes = {
+            largura: 0,
+            altura: 0,
+        }
+
+        this.classe = ""
+    }
+
+    Criar() {
+        this.elemento = document.createElement("div")
+
+        this.elemento.style.position = "absolute"
+
+        this.elemento.style.height = this.dimensoes.altura + "px"
+        this.elemento.style.width = this.dimensoes.largura + "px"
+        this.elemento.className = this.classe
+
+        document.querySelector('main').append(this.elemento)
+    }
+
+    Renderizar() {
+        this.elemento.style.top = this.posicao.y + "px";
+        this.elemento.style.left = this.posicao.x + "px";
+    }
+}
+
+class Jogador extends ObjetoDoJogo {
+    constructor() {
+        super();
+        this.posicao.x = 0;
+        this.posicao.y = 500;
+        this.dimensoes.largura = 20;
+        this.dimensoes.altura = 20;
+        this.classe = "jogador";
+
+        this.alturaPulo = 150;
+        this.yBase = 0;
+        this.velocidade = 5;
+
+        this.Criar();
+        this.AplicarQueda();
+    }
+
+    Animar() {
+        console.log(this)
+        if (this.estado === "pulando") {
+            this.posicao.y -= this.velocidade
+
+            if (this.posicao.y <= this.yBase - this.alturaPulo) {
+                this.AplicarQueda()
+            }
+        }
+
+        if (this.estado === "caindo") {
+            this.posicao.y += this.velocidade
+        }
+
+        this.elemento.style.top = this.posicao.y + "px"
+        this.elemento.style.left = this.posicao.x + "px"
+    }
+
+    AplicarPulo() {
+        this.estado = "pulando";
+        this.yBase = this.posicao.y;
+    }
+
+    AplicarQueda() {
+        this.estado = "caindo";
+    }
+}
+
+class Plataforma extends ObjetoDoJogo {
+    constructor(x, y) {
+        super(x, y)
+        this.posicao.x = x
+        this.posicao.y = y
+        this.dimensoes.largura = 40
+        this.dimensoes.altura = 3
+        this.classe = "plataforma"
+
+        this.Criar()
+    }
+}
+
+
+const jogo = new Jogo()
